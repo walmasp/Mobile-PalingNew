1a2753bc6feb25c2a22d865ca040b8185264f522
Alma Wulan <150516392+walmasp@users.noreply.github.com>
2026-05-04 17:40:12 +0700

    Update point_provider.dart

--- mobile_coffee_app/lib/core/utils/point_provider.dart
+++ mobile_coffee_app/lib/core/utils/point_provider.dart
@@ -5,9 +5,8 @@ class PointProvider with ChangeNotifier {
 
   int get poin => _poin;
 
-  // Fungsi untuk update poin di seluruh aplikasi secara instan
   void updatePoin(int poinBaru) {
     _poin = poinBaru;
-    notifyListeners(); // Ini yang membuat semua widget yang dengerin bakal refresh otomatis
+    notifyListeners(); 
   }
 }
