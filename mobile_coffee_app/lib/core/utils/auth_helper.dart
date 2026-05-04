d952e7f30601a0d247d8acb79fe03bf456538113
Alma Wulan <150516392+walmasp@users.noreply.github.com>
2026-05-04 17:38:49 +0700

    Update auth_helper.dart
    Refactor auth helper functions and add comments for clarity.

--- mobile_coffee_app/lib/core/utils/auth_helper.dart
+++ mobile_coffee_app/lib/core/utils/auth_helper.dart
@@ -1,6 +1,5 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-// Tetap biarkan fungsi ini di luar class agar booking_service dll tidak error
 Future<Map<String, String>> getAuthHeaders() async {
   final prefs = await SharedPreferences.getInstance();
   final token = prefs.getString('token');
@@ -15,7 +14,6 @@ Future<Map<String, String>> getAuthHeaders() async {
   };
 }
 
-// Tambahkan class ini untuk keperluan Login, Logout, dan CheckAuth
 class AuthHelper {
   static Future<void> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
@@ -29,7 +27,6 @@ class AuthHelper {
 
   static Future<bool> isTokenValid() async {
     final token = await getToken();
-    // Jika token ada dan tidak kosong, berarti valid
     return token != null && token.isNotEmpty;
   }
 
